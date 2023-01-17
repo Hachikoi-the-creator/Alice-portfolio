9f4c4ff2d0ed85c62acb704cf021e1a517dd826d
Hachikoi-the-creator <facka72@gmail.com>
2023-01-16 22:54:31 -0600

    final project layout fix I wish...

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -5,14 +5,12 @@ import { motion } from "framer-motion";
 
 export default function ProjectsContainer() {
   const cleanerReturns = (projectData, isTricky) => {
-    console.log("RUNNIN", projectData.title);
     const { imgUrl, title, desc, liveLink, codeLink, techStack, rowReverse } =
       projectData;
 
     return (
       <Project
         key={title}
-        className={isTricky ? "dirty-project" : ""}
         {...{ title }}
         {...{ imgUrl }}
         {...{ desc }}
@@ -25,7 +23,6 @@ export default function ProjectsContainer() {
   };
 
   // since I can't keep bothering with the dammed parallex effect I'll just add a project that will be hidden on small screens, and display none in big screens
-  const dirtyFix = projectsData.pop();
 
   return (
     <div className="main-container" id="projects">
@@ -38,8 +35,6 @@ export default function ProjectsContainer() {
       </motion.div>
       <div className="wrapper">
         {projectsData.map((project) => cleanerReturns(project))}
-
-        {cleanerReturns(dirtyFix, true)}
       </div>
     </div>
   );
